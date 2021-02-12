5b174fafe9e70a14c89858214f71a4859300bcb1
Roberto Lopez-Trigo <rlope232@fiu.edu>
2021-02-12 18:06:46 -0500

    Bobby

--- src/components/AllBeers.js
+++ src/components/AllBeers.js
@@ -1,39 +1,41 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Link}  from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
 
 function AllBeers(props) {
-        let [beers, setbeers] = useState(['🍺'])
+  let [beers, setbeers] = useState(['🍺']);
 
-        useEffect(() => {
-            axios.get('https://ih-beers-api2.herokuapp.com/beers')
-            .then(res => {
-                console.log(res)
-                setbeers(res.data)
-            })
-        } ,[]
-        )
+  const randNum = Math.floor(Math.random() * 255);
 
-            const showbeers = () => {
-                return beers.map(eachbeer => {
-                    return (
-                        <li>
-                        <Link to={`/allbeers/${eachbeer._id}`}>{eachbeer.name} </Link> <img src={eachbeer.image_url}/>
-                            
-                        </li>
-                    )
+  let style = {
+    backgroundColor: `rgb(${randNum},${randNum},${randNum})`,
+  };
 
-                })
-            }
+  useEffect(() => {
+    axios.get('https://ih-beers-api2.herokuapp.com/beers').then((res) => {
+      console.log(res);
+      setbeers(res.data);
+    });
+  }, []);
 
+  const showbeers = () => {
+    return beers.map((eachbeer) => {
+      return (
+        <li className="beerList">
+          <div style={style}>
+            <Link to={`/allbeers/${eachbeer._id}`}>{eachbeer.name} </Link>{' '}
+            <img src={eachbeer.image_url} />
+          </div>
+        </li>
+      );
+    });
+  };
 
-
-    return (
-        <div>
-            {showbeers()}
-        </div>
-    );
+  return (
+    <div style={style} className="gridBeer">
+      {showbeers()}
+    </div>
+  );
 }
 
 export default AllBeers;
